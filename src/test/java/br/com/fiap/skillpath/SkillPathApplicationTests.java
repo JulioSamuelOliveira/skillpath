c8160c91d7b3437d6d79d20572a3ab6ad9a46044
JulioSamuelOliveira <juliosamuoliveira@gmail.com>
2025-11-11 19:40:22 -0300

    project created

---
+++ src/test/java/br/com/fiap/skillpath/SkillPathApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.skillpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
